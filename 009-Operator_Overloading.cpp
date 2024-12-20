d9e0f826a01bf3251a7a91a1eccbca034ba47d42
Yashwant Choudhary <84570757+Yashwant077@users.noreply.github.com>
2024-12-20 20:56:25 +0530

    Update 009-Operator_Overloading.cpp

--- 009-Operator_Overloading.cpp
+++ 009-Operator_Overloading.cpp
@@ -66,6 +66,30 @@ class Integer {
         return temp;
     }
     
+    // comparison operator Overloading
+    bool operator ==(const Integer &obj) const {
+        return *m_pInt == *obj.m_pInt;
+    }
+    
+    // assignment operator Overloading
+    Integer & operator =(const Integer &a){
+        if(this!=&a){
+            delete m_pInt;
+            m_pInt = new int(*a.m_pInt);
+        }
+        return *this;
+    }
+    
+    // move assignment operator
+    Integer operator =(Integer &&a){
+        if(this!=&a){
+            delete m_pInt;
+            m_pInt = a.m_pInt;
+            a.m_pInt = nullptr;
+        }
+        return *this;
+    }
+    
     // Destructor
     ~Integer(){
         // cout<<"~Integer()"<<endl;
@@ -79,6 +103,27 @@ Integer Add(const Integer &a, const Integer &b){
     return temp;
 }
 
+// Global operator Overloading for 
+Integer operator +(int x, const Integer &y){
+    Integer temp;
+    temp.SetValue(x + y.GetValue());
+    return temp;
+}
+
+// insertion operator Overloading
+std::ostream & operator <<(std::ostream & out, const Integer &a){
+    out<<a.GetValue();
+    return out;
+}
+
+// extraction operator Overloading
+std::istream & operator >>(std::istream &input, Integer &a){
+    int x;
+    input>>x;
+    a.SetValue(x);
+    return input;
+}
+
 int main()
 {
     Integer a(1), b(2);
@@ -88,5 +133,42 @@ int main()
     cout<<"++sum: "<<(sum++).GetValue()<<endl;
     cout<<"sum: "<<sum.GetValue()<<endl;
     
+    if(a==b){
+        cout<<"a and b are equal"<<endl;
+    }
+    else{
+        cout<<"a and b are not equal"<<endl;
+    }
+    
+    Integer c;
+    c=a;
+    cout<<"value in c: "<<c.GetValue()<<endl;
+    
+    Integer d = c+5;
+    Integer e = 4+d;
+    
+    cout<<"d: "<<d<<endl;
+    cout<<"e: "<<e<<endl;
+    
+    std::cin>>a;
+    std::cout<<"a: "<<a<<std::endl;
+    
     return 0;
 }
+
+
+/*
+Output:
+
+sum: 3
+sum++: 4
+++sum: 4
+sum: 5
+a and b are not equal
+value in c: 1
+d: 6
+e: 10
+423
+a: 423
+
+*/
